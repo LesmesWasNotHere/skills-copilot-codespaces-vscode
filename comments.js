20fda69882539148b71df12a647410f4d2e97846
LesmesIsHere <lesmes77@gmail.com>
2024-05-31 16:08:25 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,7 @@
 //create web server
-const http = require('http');
-const port = 3000;
-const fs = require('fs');
-const path = require('path');
-//create server
-const server = http.createServer((req, res) => {
-    //check the url
-    if(req.url === '/'){
-        //set the content type
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        //read the file
-        fs.createReadStream(path.join(__dirname, 'index.html')).pipe(res);
-    }else if
+//create a web server, listen to port 3000
+//when receive a request, read the file comments.json
+//parse the file and send back to the client
+//if the file does not exist, send an error message back to the client
+//if the file is empty, send an error message back to the client
+//if the file is not a valid json file, send an error message back to
